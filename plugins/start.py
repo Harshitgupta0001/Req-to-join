40bfdc221535e91e3c40383764a69bebc8c01d00
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-02-02 13:20:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -143,7 +143,7 @@ async def start_command(client: Client, message: Message):
         ]
             ]
         )
-        await message.reply_photo(photo=https://i.ibb.co/7Lwm9CG/photo-2025-02-02-07-36-38-7466722484281147420.jpg,
+        await message.reply_photo(photo='https://i.ibb.co/7Lwm9CG/photo-2025-02-02-07-36-38-7466722484281147420.jpg',
             caption = START_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
