db8da72ce78716fabf51fe84549a67e16575b22b
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-02-06 18:54:18 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
-from plugins.users_api import get_user, update_user_info
+from plugins.users_api import get_user, update_user_info, get_short_link
 from bot import Bot
 from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
