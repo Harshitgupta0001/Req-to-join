600afd677c14f28d363fd3d014fb68593118f640
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2025-02-06 11:06:33 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -36,12 +36,13 @@ async def channel_post(client: Client, message: Message):
     if file_name != "Unknown File":
         file_name = file_name.rsplit('.', 1)[0]  # Remove the last file extension (e.g., .mkv, .mp4)
         file_name = file_name.replace('.', ' ')  # Replace dots with spaces
+        file_name = file_name.replace('_', ' ')  # Replace underscores with spaces
 
     
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("📌 GET YOUR FILE", url=f'{link}')]])
 
     await reply_text.edit(f"<b>{file_name}</b>", reply_markup=reply_markup, disable_web_page_preview = True)
-
+    await message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote = True) 
     if not DISABLE_CHANNEL_BUTTON:
         await post_message.edit_reply_markup(reply_markup)
 
